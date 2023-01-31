049380eeacba2e65a8eb44c66e708346abfa87c0
Dominik Korpusik <dominik.korpusik@gmail.com>
2023-01-31 21:27:27 +0000

    Update files

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
-4 + 3;
-if (4 + 3 === 7) {
-    alert("You're smart!")
+function sayHello() {
+    console.log("Hello");
+}
+
+sayHello()
+
+let sayBay = function () {
+    console.log("Bye!")
+}
+
+sayBay()
+
+let list = ["tiger", "cat", "bear", "bird"]
+list.shift()
+list.push("elephant")
+list.pop()
+
+
+let user = {
+    name: "John",
+    age: 34,
+    hobby: "Soccer",
+    isMarried: false,
 }
