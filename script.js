ecf421fa80d9c480d40438a58a734b7081307cc5
Dominik Korpusik <dominik.korpusik@gmail.com>
2023-01-31 22:49:46 +0000

    Upadte

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
-function sayHello() {
-    console.log("Hello");
+let database = [
+    {
+        username: "Dominik",
+        password: "super"
+    }
+];
+
+let newsFeed = [
+    {
+        username: "Bobby",
+        timeline: "So fck tired!"
+    },
+    {
+        username: "Sally",
+        timeline: "So fck tired!"
+    }
+];
+
+let userNamePrompt = prompt("What's your username?")
+let userPasswordPrompt = prompt("What's your password?")
+
+function signIn(userName, password) {
+    if (database[0].username == userNamePrompt &&
+        database[0].password == userPasswordPrompt)
+        alert("You in")
 }
 
-sayHello()
-
-let sayBay = function () {
-    console.log("Bye!")
-}
-
-sayBay()
-
-let list = ["tiger", "cat", "bear", "bird"]
-list.shift()
-list.push("elephant")
-list.pop()
-
-
-let user = {
-    name: "John",
-    age: 34,
-    hobby: "Soccer",
-    isMarried: false,
-}
+signIn()
