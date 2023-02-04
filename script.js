f9c15527416025acf9a96c06047f760bb75b85f0
Dominik Korpusik <dominik.korpusik@gmail.com>
2023-02-04 21:10:47 +0000

    Update script.js

--- script.js
+++ script.js
@@ -19,6 +19,7 @@ let newsFeed = [
 let userNamePrompt = prompt("What's your username?")
 let userPasswordPrompt = prompt("What's your password?")
 
+//Function declaration
 function signIn(userName, userPassword) {
     if (database[0].username === userName &&
         database[0].password === userPassword) {
@@ -27,5 +28,36 @@ function signIn(userName, userPassword) {
         alert("Welcome Traveler!")
     }
 }
-
 signIn(userNamePrompt, userPasswordPrompt)
+
+//Function expression 
+var newFunction = function () {
+
+};
+
+//Expression - Is something that produce a value. 
+1 + 3;
+var a = 2;
+return true;
+
+//calling or invoking a function
+alert()
+newFunction(param1, param2)
+
+//assign a variable
+var a = true;
+
+//function vs method
+function thisIsAFunction() {
+
+}
+
+var obj = {
+    thisIsAMethod: function () {
+
+    }
+}
+
+thisIsAFunction()
+obj.thisIsAMethod()
+
