908081f1a4b81d2a4431e2b0c5722129ed959ce9
Dominik Korpusik <dominik.korpusik@gmail.com>
2023-02-11 21:26:34 +0000

    Adding background generator

--- script_DOM.js
+++ script_DOM.js
@@ -1,17 +1,42 @@
 let input = document.getElementById("userInput");
 let button = document.getElementById("enter");
+let deleteButton = document.getElementsByClassName("delete");
 let ul = document.querySelector("ul");
+let li = document.querySelectorAll("li");
 
 function inputLength() {
     return input.value.length;
 }
 
 function createListElement() {
-    let li = document.createElement("li");
-    li.appendChild(document.createTextNode(input.value));
+    let created_li = document.createElement("li");
+    created_li.appendChild(document.createTextNode(input.value));
     ul.appendChild(li);
     input.value = ""
 }
+
+function deleteListElement() {
+    let created_li = document.createElement("li");
+    created_li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
+    input.value = ""
+}
+
+for (let i = 0; i < deleteButton.length; i++) {
+    const db = deleteButton[i];
+    db.addEventListener("click", function () {
+
+    })
+}
+
+
+
+// deleteButton.forEach(db => {
+//     db.addEventListener("click", function () {
+//         console.log("delete")
+//     })
+// });
+
 
 button.addEventListener("click", function () {
     if (inputLength() > 0) {
@@ -25,3 +50,16 @@ button.addEventListener("keydown", function (event) {
         createListElement();
     }
 })
+
+li.forEach(li => {
+    if (li.classList.contains("done")) {
+        li.addEventListener("click", function () {
+            li.classList.remove("done");
+        })
+    } else {
+        li.addEventListener("click", function () {
+            li.classList.add("done");
+        })
+    }
+});
+
