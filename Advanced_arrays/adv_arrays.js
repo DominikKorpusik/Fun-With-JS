b19c02dd6029f700925e32f48dfd9c9d0c28161b
Dominik Korpusik <dominik.korpusik@gmail.com>
2023-02-14 22:10:52 +0000

    Adding map, reduce, filter

--- Advanced_arrays/adv_arrays.js
+++ Advanced_arrays/adv_arrays.js
@@ -1,22 +1,31 @@
-const array = [1, 2, 10, 16];
+const array = [1, 2, 10, 16, 3, 6, 5, 44, 45, 34, 6, 754, 65, 34];
 const double = [];
+
 const newArray = array.forEach((num) => {
     double.push(num * 2);
 });
 
+console.log('array: ', array)
 console.log('forEach: ', double);
 
 //map, filter, reduce
 
-//map //new Array
-const mapArray = array.map((num) => {
-    return num * 2;
-});
+//map, new Array
+const mapArray = array.map(num => num * 2);
 
-console.log('map: ', mapArray);
+console.log('map, multiply: ', mapArray);
 
-const mapArray2 = array.map((num) => {
-    return num % 2;
-});
+const mapArray2 = array.map(num => num % 2);
 
 console.log('map, modulo: ', mapArray2);
+
+//filter, new Array
+const filterArray = array.filter(num => num % 2);
+console.log('filter, modulo: ', filterArray);
+
+//reduce, new Array
+const reduceArray = array.reduce((accumulator, num) => {
+    return accumulator + num;
+}, 0);
+
+console.log('reduce, add: ', reduceArray);
