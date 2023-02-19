c18c5ac36e1fe5be060bac5b9b889c170289db3c
Dominik Korpusik <dominik.korpusik@gmail.com>
2023-02-19 15:40:13 +0000

    Update ES2020

--- ES2020/ES2020.js
+++ ES2020/ES2020.js
@@ -1,5 +1,43 @@
+//  ES2020
+
 //  BigInt
-//  Nullish Coalescing Operrator ??
+typeof 1n //  "bigint"
+Number.MAX_SAFE_INTEGER //  9007199254740991
+
 //  Optional Chaining Operator ?.
-//  Promise.allSettled
-//  globalThis
+// Working on objects and checking their properties
+let will_pokemon = {
+    pikachu: {
+        species: 'Mouse Pokemon',
+        height: 0.4,
+        weight: 6
+    }
+}
+
+let andrei_pokemon = {
+    raichu: {
+        species: 'Mouse Pokemon',
+        height: 0.8,
+        weight: 30
+    },
+    pikachu: {
+        species: 'Mouse Pokemon',
+        height: 0.4,
+        weight: 6,
+        power: 'lightning'
+    }
+}
+
+if (andrei_pokemon.pikachu && andrei_pokemon.pikachu.weight) {
+    let weight2 = andrei_pokemon.pikachu.weight;
+} else {
+    let weight2 = undefined;
+}
+
+let weight = andrei_pokemon?.pikachu?.weight;
+console.log(weight)
+
+//  Nullish Coalescing Operrator ??
+// It's checks if its null or undefinded
+let power = andrei_pokemon?.pikachu?.power ?? 'no power'
+
